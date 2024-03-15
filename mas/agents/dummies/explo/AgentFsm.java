b6be25cf6c4fe6c1a997ab61c2b8da4a1eb2caea
Kim Saïdi <95487587+KIMSAIDI@users.noreply.github.com>
2024-03-15 16:55:21 +0100

    first commit

---
+++ mas/agents/dummies/explo/AgentFsm.java
@@ -0,0 +1,58 @@
+package eu.su.mas.dedaleEtu.mas.agents.dummies.explo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.su.mas.dedale.mas.AbstractDedaleAgent;
+import eu.su.mas.dedale.mas.agent.behaviours.platformManagment.*;
+
+import eu.su.mas.dedaleEtu.mas.behaviours.ExploCoopBehaviour;
+import eu.su.mas.dedaleEtu.mas.behaviours.SayHelloBehaviour;
+import eu.su.mas.dedaleEtu.mas.knowledge.MapRepresentation;
+
+import jade.core.behaviours.Behaviour;
+import main.java.eu.su.mas.dedaleEtu.mas.behaviours.ReceivePing;
+
+/**
+ * <pre>
+ * ExploreCoop agent. 
+ * Basic example of how to "collaboratively" explore the map
+ *  - It explore the map using a DFS algorithm and blindly tries to share the topology with the agents within reach.
+ *  - The shortestPath computation is not optimized
+ *  - Agents do not coordinate themselves on the node(s) to visit, thus progressively creating a single file. It's bad.
+ *  - The agent sends all its map, periodically, forever. Its bad x3.
+ *  - You should give him the list of agents'name to send its map to in parameter when creating the agent.
+ *   Object [] entityParameters={"Name1","Name2};
+ *   ag=createNewDedaleAgent(c, agentName, ExploreCoopAgent.class.getName(), entityParameters);
+ *  
+ * It stops when all nodes have been visited.
+ * 
+ * 
+ *  </pre>
+ *  
+ * @author hc
+ *
+ */
+
+
+public class AgentFsm extends Agents {
+	// States
+	private static final String A = "A";
+	private static final String B = "B";
+	private static final String C = "C";
+	private static final String D = "D";
+	protected void setup() {
+		FSMBehaviour fsm = new FSMBehaviour(this);
+		// Define the different states and behaviours
+		fsm.registerFirstState(new ExploCoopBehaviour(this, this.map), A);
+		fsm.registerState(new SayHelloBehaviour(this, this.map), B);
+		fsm.registerState(new ShareMapBehaviour(this, this.map), C);
+		fsm.registerState(new ReceivePing(this, this.map), D);
+		// Define the transitions
+		fsm.registerDefaultTransition(A, B);
+		fsm.registerDefaultTransition(B, C);
+		fsm.registerDefaultTransition(C, A);
+		addBehaviour(fsm);
+	}
+
+}
